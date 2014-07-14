23736d76d876e7c7b2dd559180ddbae28a3def3d
T Wallace <tylerwallace@homepc>
2014-07-13 22:11:47 -0700

    Added main menu and preliminary sound system.

---
+++ Assets/Scripts/MusicManagerScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManagerScript : _Mono {
+
+	public AudioClip[] Music;
+
+	private List<AudioClip> musicClips;
+	private bool paused = false;  // Idicates if the music has been manually paused or stopped.
+
+	public void Start () {
+		musicClips = new List<AudioClip>();
+		foreach(AudioClip ac in Music)
+			musicClips.Add(ac);
+	}
+
+	public void AddMusicClip(AudioClip clip){
+		musicClips.Add(clip);
+	}
+
+	public void ClearMusicClips(){
+		musicClips.Clear();
+	}
+	
+	public void PauseMusic() {
+		audio.Pause();
+		paused = true;
+	}
+
+	public void StopMusic() {
+		audio.Stop();
+		paused = true;
+	}
+
+	public void PlayMusic() {
+		audio.Play();
+		paused = false;
+	}
+
+	public void Update(){
+		if(paused || audio.isPlaying) { // We were told to stop playing or the current clip is still playing
+			return;
+		}
+		audio.clip = musicClips[Random.Range(0, musicClips.Count)];
+		if(audio.clip != null)
+			audio.Play();
+	}
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public static class Sound {
+
+	private static GameObject _cam;
+	private static GameObject cam {
+		get {
+			if(_cam == null) {
+				_cam = GameObject.Find("SpecialCamera");
+			}
+			return _cam;
+		}
+	}
+
+	public static void PlaySound(AudioClip sound) {
+		Vector3 point;
+		if(cam != null)
+			point = cam.transform.position;
+		else
+			point = new Vector3(0,0,0);
+		AudioSource.PlayClipAtPoint(sound, point);
+		// TODO: add volume adjustment from options
+	}
+
+	public static void PauseMusic() {
+		if(cam == null) {
+			Debug.LogError("An object named SpecialCamera could not be found in the scene.");
+			return;
+		}
+		cam.GetComponent<MusicManagerScript>().PauseMusic();
+	}
+
+	public static void PlayMusic() {
+		if(cam == null) {
+			Debug.LogError("An object named SpecialCamera could not be found in the scene.");
+			return;
+		}
+		cam.GetComponent<MusicManagerScript>().PlayMusic();
+	}
+
+	public static void StopMusic() {
+		if(cam == null) {
+			Debug.LogError("An object named SpecialCamera could not be found in the scene.");
+			return;
+		}
+		cam.GetComponent<MusicManagerScript>().StopMusic();
+	}
+
+}
