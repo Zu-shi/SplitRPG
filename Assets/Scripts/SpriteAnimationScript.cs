9bb6d4a59ffb4bf62652190d7a1480f15b2f4840
T Wallace <wallacet@email.arizona.edu>
2014-07-22 20:28:13 -0700

    Added tooltips to the sprite animation system and the music/sound effects system.

--- Assets/Scripts/SpriteAnimationScript.cs
+++ Assets/Scripts/SpriteAnimationScript.cs
@@ -4,8 +4,15 @@ using System.Collections;
 // A simple data carrier. Used by SpriteAnimationManager components.
 public class SpriteAnimationScript : _Mono {
 
+	[Tooltip("The name that is used to reference this animation, i.e. idle, walking, etc.")]
 	public string animationName = "Animation";
-	public float rate = 10; // Frames per second
+
+	[Tooltip("The rate, in frames per second, at which this animation should be played.")]
+	public float rate = 10;
+
+	[Tooltip("If set, this animation will loop when it is played.")]
 	public bool loop = true;
+
+	[Tooltip("The sequence of sprites that make up this animation.")]
 	public Sprite[] sprites;
 }
