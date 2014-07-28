f91387e4f92f744c00db44dc69386212a15993aa
Mark Gardner <m15399@gmail.com>
2014-07-27 18:47:43 -0700

    Created a MovementScript class that CharacterMovementScript inherits from, has a canPush option so it can be used on blocks and other stuff

--- Assets/Scripts/PushBlockColliderScript.cs
+++ Assets/Scripts/PushBlockColliderScript.cs
@@ -3,20 +3,15 @@ using System.Collections;
 
 public class PushBlockColliderScript : ColliderScript {
 	
+	MovementScript movementScript;
+
+	void Start(){
+		movementScript = GetComponent<MovementScript>();
+	}
+
 	public override bool TryToPush(GameObject pusher, Direction dir){
-		// See if we can be pushed in a direction
-		Vector2 targetLoc = tileVector + 2 * Utils.DirectionToVector(dir);
-		bool blocked = Globals.CollisionManager.TileBlocking(targetLoc);
 
-		if(blocked){
-			// Can't be pushed because we're blocked
-			return false;
-
-		} else {
-			// Push ourselves to the new location
-			tileVector = targetLoc;
-			return true;
-		}
+		return movementScript.MoveInDirection(dir);
 	}
 
 }
