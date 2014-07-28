7a5fc01043130dbe947782050e119f0055c902a1
Mark Gardner <m15399@gmail.com>
2014-07-28 15:36:31 -0700

    Redid Switches (now called Buttons) and Gates to use new Collision Manager, removed some unused prefabs

--- Assets/Scripts/GateScript.cs
+++ Assets/Scripts/GateScript.cs
@@ -15,7 +15,11 @@ public class GateScript : _Mono {
 	[Tooltip("Make the gate OPEN when its switch is off.")]
 	public bool reverse = false;
 
+	ColliderScript colliderScript;
 
+	void Start(){
+		colliderScript = GetComponent<ColliderScript>();
+	}
 
 	void Update () {
 		bool open = switchToWatch.on;
@@ -24,10 +28,13 @@ public class GateScript : _Mono {
 
 		if(open){
 			spriteRenderer.sprite = openSprite;
-			collider2D.isTrigger = true; // disable collision
+			// disable collision
+			colliderScript.blocking = false;
 		} else {
 			spriteRenderer.sprite = closedSprite;
-			collider2D.isTrigger = false; // enable collision
+			// enable collision
+			colliderScript.blocking = true;
+
 		}
 	}
 }
