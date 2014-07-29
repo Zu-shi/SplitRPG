4b9d845cf696da5f66d5ca332c246cddcb71bdeb
T Wallace <wallacet@email.arizona.edu>
2014-07-29 16:34:33 -0700

    Level manager can load both left and right levels.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,64 +8,120 @@ public class LevelManager : _Mono{
 
 	private List<GameObject> _levelPrefabs;
 
-	private GameObject currentLevelPrefab;
+	private GameObject currentLeftLevelPrefab, currentRightLevelPrefab;
+	private Transform leftSpawn, rightSpawn;
 
-	public string currentLevel {
+	public string currentLeftLevel {
 		get {
-			if(currentLevelPrefab != null) {
-				return currentLevelPrefab.name;
+			if(currentLeftLevelPrefab != null) {
+				return currentLeftLevelPrefab.name;
 			}
 			else
 				return null;
 		}
 	}
 
-	public bool LoadLevel(string name) {
-		for(int i = 0; i < _levelPrefabs.Count; i++) {
-			if(_levelPrefabs[i].name == name) {
-				return LoadLevel(_levelPrefabs[i]);
+	public string currentRightLevel {
+		get {
+			if(currentRightLevelPrefab != null) {
+				return currentRightLevelPrefab.name;
 			}
+			else
+				return null;
 		}
-		return false;
 	}
 
-	public bool LoadLevel(GameObject prefab) {
-		GameObject go = (GameObject)GameObject.Instantiate(prefab);
-
-		if(go != null) {
-			GameObject.DestroyImmediate(currentLevelPrefab);
-			currentLevelPrefab = go;
-			
-			GameObject spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint");
-			if(spawnPoint != null) {
-				Globals.cameraLeft.FadeTransition(MovePlayersToSpawnPoint, FinishedLoading);
-				Globals.cameraRight.FadeTransition(MovePlayersToSpawnPoint, FinishedLoading);
+	public bool LoadLevels(string leftLevelName, string rightLevelName) {
+		GameObject left = null;
+		GameObject right = null;
+		for(int i = 0; i < _levelPrefabs.Count; i++) {
+			if(_levelPrefabs[i].name == leftLevelName) {
+				left = _levelPrefabs[i];
 			}
-			else {
-				Debug.LogError("No spawn point set for level: " + prefab.name);
+			if(_levelPrefabs[i].name == rightLevelName) {
+				right = _levelPrefabs[i];
 			}
-
-			return true;
+		}
+		if(left != null && right != null) {
+			return LoadLevels(left, right);
 		}
 		else{
-			Debug.LogError("Failed to instantiate level prefab");
 			return false;
 		}
 	}
 
-	private void MovePlayersToSpawnPoint() {
-		GameObject spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint");
-		GameObject.FindGameObjectWithTag("PlayerLeft").transform.position = spawnPoint.transform.position;
-		GameObject.FindGameObjectWithTag("PlayerRight").transform.position = spawnPoint.transform.position;
-		Globals.roomManager.MoveCamerasToPoint( new Vector2(spawnPoint.transform.position.x, spawnPoint.transform.position.y));
+	public bool LoadLevels(GameObject leftLevel, GameObject RightLevel) {
+		GameObject left = (GameObject)GameObject.Instantiate(leftLevel, Vector3.zero, Quaternion.identity);
+		GameObject right = (GameObject)GameObject.Instantiate(RightLevel, Vector3.zero, Quaternion.identity);
+		left.SetActive(false);
+		right.SetActive(false);
+		Globals.playerLeft.gameObject.SetActive(false);
+		Globals.playerRight.gameObject.SetActive(false);
+
+		Globals.roomManager.enabled = false;
+		// Left level
+		if(left != null) {
+			GameObject.DestroyImmediate(currentLeftLevelPrefab);
+			currentLeftLevelPrefab = left;
+
+			leftSpawn = left.transform.FindChild("SpawnPoint");
+			if(leftSpawn != null) {
+				Globals.cameraLeft.FadeTransition(MoveLeftCharacterToSpawnPoint, FinishedLoading);
+			}
+			else {
+				Debug.LogError("No spawn point set for level: " + left.name);
+			}
+		}
+		else{
+			Debug.LogError("Failed to instantiate level prefab");
+			Globals.roomManager.enabled = true;
+			return false;
+		}
+
+		// Right level
+		if(right != null) {
+			GameObject.DestroyImmediate(currentRightLevelPrefab);
+			currentRightLevelPrefab = right;
+			
+			rightSpawn = right.transform.FindChild("SpawnPoint");
+			if(rightSpawn != null) {
+				Globals.cameraRight.FadeTransition(MoveRightCharacterToSpawnPoint, FinishedLoading);
+			}
+			else {
+				Debug.LogError("No spawn point set for level: " + right.name);
+			}
+		}
+		else{
+			Debug.LogError("Failed to instantiate level prefab");
+			Globals.roomManager.enabled = true;
+			return false;
+		}
+
+		return true;
+
+	}
+
+	private void MoveLeftCharacterToSpawnPoint() {
+		currentLeftLevelPrefab.SetActive(true);
+		Globals.playerLeft.gameObject.SetActive(true);
+		GameObject.FindGameObjectWithTag("PlayerLeft").transform.position = leftSpawn.position;
+	}
+
+	private void MoveRightCharacterToSpawnPoint() {
+		currentRightLevelPrefab.SetActive(true);
+		Globals.playerRight.gameObject.SetActive(true);
+		GameObject.FindGameObjectWithTag("PlayerRight").transform.position = rightSpawn.position;
+		Globals.roomManager.MoveCamerasToPoint( new Vector2(rightSpawn.transform.position.x, rightSpawn.transform.position.y));
+
 	}
 
 	public void FinishedLoading() {
 		Debug.Log("LevelManagerCallBack");
+		Globals.roomManager.enabled = true;
 	}
 
 	private void _Load() {
-		LoadLevel(_levelPrefabs[0]);
+		LoadLevels(_levelPrefabs[0], _levelPrefabs[1]);
 	}
 
 	public void Start() {
@@ -74,10 +130,10 @@ public class LevelManager : _Mono{
 			_levelPrefabs.Add(levelPrefabs[i]);
 		}
 
-		if(levelPrefabs.Length > 0) {
+		if(levelPrefabs.Length > 1) {
 			Invoke( "_Load" , 0.5f );
 		}
 		else
-			Debug.LogError("No level prefabs assigned!");
+			Debug.LogError("Not enough level prefabs assigned!");
 	}
 }
