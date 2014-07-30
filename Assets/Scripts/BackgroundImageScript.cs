2e74fea87c3b87c8b50902d6744892a0b66e66cc
Mark Gardner <Mark@Macintosh-6.local>
2014-07-29 21:34:38 -0700

    Added BackgroundImageScript and a test background, also Cindy's animation

---
+++ Assets/Scripts/BackgroundImageScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class BackgroundImageScript : _Mono {
+
+	public float parralaxDepth = 1;
+	public bool runInEditor = false;
+
+	bool started = false;
+	Vector3 lastCameraPosition;
+
+	void Update () {
+		if(!Application.isPlaying && !runInEditor){
+			return;
+		}
+
+		CameraScript cameraScript = Globals.cameraLeft;
+		if(Utils.LayerIs(gameObject.layer, "Right")){
+			cameraScript = Globals.cameraRight;
+		}
+
+		if(!started){
+			lastCameraPosition = cameraScript.transform.position;
+			started = true;
+		}
+
+		if(cameraScript.transform.position != lastCameraPosition){
+			Vector3 diff = cameraScript.transform.position - lastCameraPosition;
+			float fac = - 1 / parralaxDepth;
+
+			// localPosition should keep scaling things down from effecting the look too much
+			transform.localPosition += fac * diff;
+		}
+
+		lastCameraPosition = cameraScript.transform.position;
+	}
+
+
+}
