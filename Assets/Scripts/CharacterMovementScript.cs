8f20929d30c5de263bed91bffaaa44a104a304ab
Zuoming <Zuoming@Zuomings-MacBook-Air.local>
2014-07-30 19:59:02 -0700

    Added more detail to GUI and added sound effects

--- Assets/Scripts/CharacterMovementScript.cs
+++ Assets/Scripts/CharacterMovementScript.cs
@@ -4,11 +4,21 @@ using System.Collections;
 public class CharacterMovementScript : MovementScript {
 
 	public AudioClip walkingSound;
-
+	public AudioClip wrongBeep;
+	private const float wrongBeatWaitTime = 1.0f;
 
 	protected override void StartMoving( Vector2 velocity){
 		base.StartMoving (velocity);
 		Globals.soundManager.PlaySound (walkingSound);
+		//return yield (WaitForSeconds (1.0f));
 	}
 
+	protected void PlayWrongBeep(){
+		Globals.soundManager.PlaySound (wrongBeep);
+	}
+
+	protected override void StartFall(){
+		base.StartFall ();
+		Invoke("PlayWrongBeep", 0.6f);
+	}
 }
