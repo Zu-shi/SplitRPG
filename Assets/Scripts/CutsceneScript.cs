c467d691d46caab874b0d42a0bd10c7b5e36c43c
T Wallace <wallacet@email.arizona.edu>
2014-09-21 14:24:00 -0700

    Buggy cutscenes

---
+++ Assets/Scripts/CutsceneScript.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// To use this class, override ActionSequence with a series of calls of methods in this class.
+public abstract class CutsceneScript : _Mono {
+	protected GameObject leftPlayer, rightPlayer;
+
+	public bool triggered = false;
+
+	public void Start() {
+		leftPlayer = Globals.playerLeft.gameObject;
+		rightPlayer = Globals.playerRight.gameObject;
+	}
+
+	public void Update() {
+		if(triggered)
+			return;
+
+		if(Globals.collisionManager.IsPlayerOnTile(tileVector, gameObject.layer)) {
+			triggered = true;
+			Begin();
+		}
+	}
+
+	protected Utils.VoidDelegate callback;
+
+	public virtual IEnumerator Begin(Utils.VoidDelegate callback = null) {
+		this.callback = callback;
+		Globals.gameManager.GetComponent<PlayerInputScript>().enabled = false;
+
+		yield return StartCoroutine(DumbActionSequence());
+	}
+
+	private IEnumerator DumbActionSequence() {
+		ActionSequence();
+		yield return 0;
+	}
+
+	protected abstract void ActionSequence();
+
+	protected float Move(GameObject mover, Direction dir, int numTiles = 1) {
+		MovementScript ms = mover.GetComponent<MovementScript>();
+		if(ms == null) {
+			return 0.0f;
+		}
+
+		StartCoroutine(MoveCoroutine(ms, dir, numTiles));
+		return numTiles * 14 * (1.0f/50);
+		// ^^ numTiles * numFramesPerMove * lengthOfFrameInSeconds ^^
+	}
+
+	private IEnumerator MoveCoroutine(MovementScript ms, Direction dir, int numTiles) {
+		while(numTiles > 0) {
+			if(ms.isMoving) {
+				yield return null;
+				continue;
+			}
+			ms.MoveInDirection(dir);
+			numTiles--;
+		}
+		yield return 0;
+	}
+
+	// This should be the only blocking method.
+	protected void Wait(float timeInSeconds) {
+		DumbWait(timeInSeconds);
+	}
+
+	private IEnumerator DumbWait(float timeInSeconds) {
+		Debug.Log("Starting dumb wait.");
+		yield return new WaitForSeconds(timeInSeconds);
+	}
+
+
+	protected void End() {
+		Globals.gameManager.GetComponent<PlayerInputScript>().enabled = true;
+		if(callback != null) {
+			callback();
+		}
+	}
+}
---
+++ Assets/Scripts/Cutscenes/TestCutsceneScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCutsceneScript : CutsceneScript {
+	
+	protected override void ActionSequence() {
+		float waitTime = 0;
+		waitTime = Move(leftPlayer, Direction.UP, 2);
+		Debug.Log("Waiting...");
+		Wait(waitTime);
+		waitTime = Move(rightPlayer, Direction.UP, 2);
+		Debug.Log("Waiting...");
+		Wait(waitTime);
+		Debug.Log("Ending...");
+		End();
+	}
+}
