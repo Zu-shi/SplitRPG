cf59164963c401e3995757bd0e7a50537c7d0ccb
T Wallace <wallacet@email.arizona.edu>
2014-09-21 17:50:04 -0700

    Non-buggy cutscenes. Also updated the gitignore.

--- Assets/Scripts/CutsceneScript.cs
+++ Assets/Scripts/CutsceneScript.cs
@@ -2,17 +2,34 @@
 using System.Collections;
 
 // To use this class, override ActionSequence with a series of calls of methods in this class.
+/// <summary>
+/// Base class for all cutscenes. Clients should override the ActionSequence method with the events
+/// of the cutscene, using the other methods of this class.
+/// Note that this class caches references to both player characters and both cameras for ease of use.
+/// 
+/// By default, cutscenes are triggered when a player collides with the cutscene's tile. Override the
+/// update function to disable or change this behavior.
+/// </summary>
+/// <author>Tyler Wallace</author>
 public abstract class CutsceneScript : _Mono {
-	protected GameObject leftPlayer, rightPlayer;
 
+	protected GameObject			leftPlayer, rightPlayer;
+	protected CameraScript			leftCamera, rightCamera;
+	protected Utils.VoidDelegate	callback;
+
+	/// <summary>
+	/// Has this cutscene been triggered yet?
+	/// </summary>
 	public bool triggered = false;
 
 	public void Start() {
 		leftPlayer = Globals.playerLeft.gameObject;
 		rightPlayer = Globals.playerRight.gameObject;
+		leftCamera = Globals.cameraLeft;
+		rightCamera = Globals.cameraRight;
 	}
 
-	public void Update() {
+	public virtual void Update() {
 		if(triggered)
 			return;
 
@@ -22,26 +39,53 @@ public abstract class CutsceneScript : _Mono {
 		}
 	}
 
-	protected Utils.VoidDelegate callback;
-
-	public virtual IEnumerator Begin(Utils.VoidDelegate callback = null) {
+	/// <summary>
+	/// Begin the cutscene and store the specified callback.
+	/// </summary>
+	/// <param name="callback">Callback.</param>
+	public virtual void Begin(Utils.VoidDelegate callback = null) {
 		this.callback = callback;
 		Globals.gameManager.GetComponent<PlayerInputScript>().enabled = false;
 
-		yield return StartCoroutine(DumbActionSequence());
+		StartCoroutine("ActionSequence");
 	}
 
-	private IEnumerator DumbActionSequence() {
-		ActionSequence();
-		yield return 0;
-	}
+	/// <summary>
+	/// The meat of the cutscene. Derived classes implement this to determine what happens in the cutscene.
+	/// The implementation of this should rely heavily on the other methods of this class, namely:
+	/// 	Move,
+	/// 	FadeCamOut,
+	/// 	FadeCamIn,
+	/// 	ShowSpeechBubble (TODO),
+	/// 	HideSpeechBubble (TODO)
+	/// 
+	/// IMPORTANT: The End() needs to be called at the end of the sequence, else clean up code and callbacks
+	/// will not be run.
+	/// </summary>
+	/// <returns>The sequence.</returns>
+	protected abstract IEnumerator ActionSequence();
 
-	protected abstract void ActionSequence();
-
+	/// <summary>
+	/// Move the specified mover in the direction dir for numTiles.
+	/// If numTiles = 0 and the mover is a character, the mover will face dir.
+	/// </summary>
+	/// <param name="mover">Mover.</param>
+	/// <param name="dir">Dir.</param>
+	/// <param name="numTiles">Number tiles.</param>
+	/// <returns>The amount of time it will take to finish this movement.</returns>
 	protected float Move(GameObject mover, Direction dir, int numTiles = 1) {
 		MovementScript ms = mover.GetComponent<MovementScript>();
-		if(ms == null) {
+		if(ms == null || dir == Direction.NONE) {
 			return 0.0f;
+		}
+
+		if(numTiles == 0) {
+			// Can we face in the given direction? Only characters do that.
+			if(ms.GetType() == typeof(CharacterMovementScript)) {
+				CharacterMovementScript cms = ms as CharacterMovementScript;
+				StartCoroutine(ChangeDirectionCoroutine(cms, dir));
+			}
+			return 0.0f; // Not exactly true, but close enough.
 		}
 
 		StartCoroutine(MoveCoroutine(ms, dir, numTiles));
@@ -58,24 +102,69 @@ public abstract class CutsceneScript : _Mono {
 			ms.MoveInDirection(dir);
 			numTiles--;
 		}
-		yield return 0;
 	}
 
-	// This should be the only blocking method.
-	protected void Wait(float timeInSeconds) {
-		DumbWait(timeInSeconds);
+	private IEnumerator ChangeDirectionCoroutine(CharacterMovementScript cms, Direction dir) {
+		while(cms.isMoving || cms.isChangingDirection) {
+			yield return null;
+		}
+		cms.ChangeDirection(dir);
 	}
 
-	private IEnumerator DumbWait(float timeInSeconds) {
-		Debug.Log("Starting dumb wait.");
-		yield return new WaitForSeconds(timeInSeconds);
-	}
-
-
+	/// <summary>
+	/// End the cutscene with any clean up code and call the callback.
+	/// </summary>
 	protected void End() {
 		Globals.gameManager.GetComponent<PlayerInputScript>().enabled = true;
 		if(callback != null) {
 			callback();
 		}
 	}
+
+	/// <summary>
+	/// Fades the passed camera out.
+	/// </summary>
+	/// <returns>Estimated time this fade out will take.</returns>
+	/// <param name="cam">Cam.</param>
+	protected float FadeCameraOut(CameraScript cam) {
+		cam.BeginFadeDown();
+		return cam.fader.EstimateTime();
+	}
+
+	/// <summary>
+	/// Fades the passed camera in.
+	/// </summary>
+	/// <returns>Estimated time this fade in will take.</returns>
+	/// <param name="cam">Cam.</param>
+	protected float FadeCameraIn(CameraScript cam) {
+		cam.BeginFadeUp();
+		return cam.fader.EstimateTime();
+	}
+
+	protected GameObject ShowSpeechBubble(GameObject speaker, GameObject bubblePrefab) {
+		if(speaker == null) {
+			Debug.LogError("Cannot show bubble without a speaker.");
+			return null;
+		}
+		return ShowSpeechBubble(speaker.transform.position, bubblePrefab);
+	}
+
+	protected GameObject ShowSpeechBubble(Vector3 pos, GameObject bubblePrefab) {
+		if(bubblePrefab == null) {
+			Debug.LogError("Cannot show null bubble prefab.");
+			return null;
+		}
+		GameObject bubbleInstance = Instantiate(bubblePrefab, pos, Quaternion.identity) as GameObject;
+		bubbleInstance.name = bubblePrefab.name;
+		return bubbleInstance;
+	}
+
+	protected void HideSpeechBubble(GameObject bubbleInstance) {
+		if(bubbleInstance == null) {
+			Debug.LogError("Cannot hide null bubble instance.");
+			return;
+		}
+		Destroy(bubbleInstance);
+	}
+	
 }
--- Assets/Scripts/Cutscenes/TestCutsceneScript.cs
+++ Assets/Scripts/Cutscenes/TestCutsceneScript.cs
@@ -1,17 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Test cutscene script.
+/// </summary>
+/// <author>Tyler Wallace</author>
 public class TestCutsceneScript : CutsceneScript {
-	
-	protected override void ActionSequence() {
+
+	public GameObject testBubble1;
+	public GameObject testBubble2;
+
+	protected override IEnumerator ActionSequence() {
+		// Init
 		float waitTime = 0;
+		if(testBubble1 == null) {
+			Debug.LogError("No speech bubble assigned!");
+		}
+		if(testBubble2 == null) {
+			Debug.LogError("No speech bubble assigned!");
+		}
+
+		// Both characters walk up two tiles
 		waitTime = Move(leftPlayer, Direction.UP, 2);
-		Debug.Log("Waiting...");
-		Wait(waitTime);
-		waitTime = Move(rightPlayer, Direction.UP, 2);
-		Debug.Log("Waiting...");
-		Wait(waitTime);
-		Debug.Log("Ending...");
+		waitTime = Move(rightPlayer, Direction.UP, 2);	// Clobbers other wait time. Use max for better results.
+		yield return new WaitForSeconds(waitTime);		// We wait until the characters finish moving.
+
+		// Both characters walk right 4 tiles
+		waitTime = Move(leftPlayer, Direction.RIGHT, 4);
+		waitTime = Move(rightPlayer, Direction.RIGHT, 4);
+		yield return new WaitForSeconds(waitTime);
+
+		// Both characters face up
+		Move(leftPlayer, Direction.UP, 0);
+		Move(rightPlayer, Direction.UP, 0);
+
+		// Fade the cameras out
+		waitTime = FadeCameraOut(leftCamera);
+		waitTime = FadeCameraOut(rightCamera);
+		yield return new WaitForSeconds(waitTime);
+
+		// Both characters move right 2 tiles
+		waitTime = Move(leftPlayer, Direction.LEFT, 2);
+		waitTime = Move(rightPlayer, Direction.LEFT, 2);
+		yield return new WaitForSeconds(waitTime);
+
+		// Both characters face down
+		Move(leftPlayer, Direction.DOWN, 0);
+		Move(rightPlayer, Direction.DOWN, 0);
+		yield return new WaitForSeconds(0.5f);
+
+		// Fade the cameras in
+		waitTime = FadeCameraIn(leftCamera);
+		waitTime = FadeCameraIn(rightCamera);
+		yield return new WaitForSeconds(waitTime);
+		
+		// Left character "talks" for a short time
+		GameObject bubble1 = ShowSpeechBubble(leftPlayer, testBubble1);
+		yield return new WaitForSeconds(1.7f);
+		
+		// Left character stops talking
+		HideSpeechBubble(bubble1);
+		yield return new WaitForSeconds(0.2f);
+
+		// Right character "talks" for a short time
+		GameObject bubble2 = ShowSpeechBubble(rightPlayer, testBubble2);
+		yield return new WaitForSeconds(1.7f);
+		
+		// Left character stops talking
+		HideSpeechBubble(bubble2);
+		yield return new WaitForSeconds(0.2f);
+
+		// End the cutscene. This must be called at the end of every cutscene.
 		End();
 	}
 }
